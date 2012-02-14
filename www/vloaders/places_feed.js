b753b7f015fa771b9ae63510162a4646f3fc5967
Greg Nicholas <greg.nicholas@gmail.com>
2012-02-14 13:15:52 -0500

    places feed ready, also included missing files

--- www/vloaders/places_feed.js
+++ www/vloaders/places_feed.js
@@ -3,21 +3,40 @@
 vloaders.places_feed = {
     init: function() {},
     render: function(element,data) {
-        var postrender = function(html) {
-            var home_link = $('<a id="home-link" href="#">Home</a>');
-            home_link.click( function(event) {
-                    load_view('home',element)
-                    event.preventDefault();
-                });
-                
-            element.html(home_link);
-            element.append(html);
+        // helper function that binds view loaders to the current element
+        var click_bind = function(link,vloader,data) {
+            link.click( function(event) {
+                load_view(vloader,element,data)
+                event.preventDefault();
+            });
         }
 
-        // simple: grab data and render single template
-        $.getJSON('http://127.0.0.1:8000/ajax/places_feed?callback=?',
-            function(json) {
-                render_template('places/feed',json,postrender);
-            });
+        var to_placelist = function(html) {
+            element.find('#content').html(html);
+        }
+
+        var to_mainnav = function(html) {
+            element.find('#main-nav').html(html);
+            click_bind($('#navlink-hot'),'home');
+            click_bind($('#navlink-places'),'places_feed');
+            click_bind($('#navlink-events'),'events_feed');
+            click_bind($('#navlink-specials'),'specials_feed');
+            // click_bind($('#navlink-chatter'),'chatter_feed');
+            // click_bind($('#navlink-news'),'news_feed');
+            // click_bind($('#navlink-jobs'),'jobs_feed');
+        }
+
+        var to_feedcontainer = function(html) {
+            element.html(html); // fills container
+
+            $.getJSON('http://127.0.0.1:8000/ajax/places_feed?callback=?',
+                function(json) {
+                    render_template('places/feed',json,to_placelist);
+                });
+
+            render_template('main_nav',{},to_mainnav);
+        }
+
+        render_template('feed_container',{},to_feedcontainer);
     }
 };
