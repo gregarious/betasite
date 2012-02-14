81387383e28e91f9953a46900680b84c4d9e7688
Greg Nicholas <greg.nicholas@gmail.com>
2012-02-14 14:21:49 -0500

    events plumming done

--- www/vloaders/events_feed.js
+++ www/vloaders/events_feed.js
@@ -1,35 +1,56 @@
 // simple view: gets JSON for events from server and fills whole element with
 // template for them
+
 vloaders.events_feed = {
     init: function() {},
     render: function(element,data) {
+        // helper function that binds view loaders to the current element
+        var bind_navclick = function(link,vloader) {
+            link.click( function(event) {
+                load_view(vloader,element)
+                event.preventDefault();
+            });
+        }
+
         // click handler for single event items
         var item_clicked = function(event) {
-            load_view('events_single',element,{'id':event.data.eid});
+            load_view('events_item',element,{'id':event.data.id});
             event.preventDefault();        
         }
 
-        var postrender = function(html,data) {
-            var home_link = $('<a id="home-link" href="#">Home</a>');
-            home_link.click( function(event) {
-                    load_view('home',element)
-                    event.preventDefault();
-                });
-                
-            element.html(home_link);
-            element.append(html);
+
+        var to_eventlist = function(html,data) {
+            element.find('#content').html(html);
 
             for (var i = data.events.length - 1; i >= 0; i--) {
                 var eid = data.events[i].id;
-                element.find('#event-'+eid).click(
-                    {'eid':eid},item_clicked);
+                element.find('#event-item-'+eid).click(
+                    {'id':eid},item_clicked);
             };
         }
 
-        // simple: grab data and render single template
-        $.getJSON('http://127.0.0.1:8000/ajax/events_feed?callback=?',
-            function(json) {
-                render_template('events/feed',json,postrender);
-            });
+        var to_mainnav = function(html) {
+            element.find('#main-nav').html(html);
+            bind_navclick($('#navlink-hot'),'home');
+            bind_navclick($('#navlink-places'),'places_feed');
+            bind_navclick($('#navlink-events'),'events_feed');
+            bind_navclick($('#navlink-specials'),'specials_feed');
+            // bind_navclick($('#navlink-chatter'),'chatter_feed');
+            // bind_navclick($('#navlink-news'),'news_feed');
+            // bind_navclick($('#navlink-jobs'),'jobs_feed');
+        }
+
+        var to_feedcontainer = function(html) {
+            element.html(html); // fills container
+
+            $.getJSON('http://127.0.0.1:8000/ajax/events_feed?callback=?',
+                function(json) {
+                    render_template('events/feed',json,to_eventlist);
+                });
+
+            render_template('main_nav',{},to_mainnav);
+        }
+
+        render_template('feed_container',{},to_feedcontainer);
     }
 };
