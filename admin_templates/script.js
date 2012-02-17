d4fd6f5e0251c84ffee6643bc6f64c8bcb6e6515
laras126 <laras126@gmail.com>
2012-02-17 15:26:17 -0500

    Add edit profile link, image classes

--- admin_templates/script.js
+++ admin_templates/script.js
@@ -1,7 +1,6 @@
 $(document).ready( function(){
 
-	console.log('loaded');
-	// helper function that binds view loaders to the current element
+	// slideToggle the add event/special forms
 	var expand_form = function(title, content) {
 	    $(title).click(function(){
 			$(content).slideToggle(500);
@@ -10,10 +9,5 @@ $(document).ready( function(){
 
 	expand_form('#openEventForm','#eventForm');
 	expand_form('#openSpecialForm','#specialForm');
-
-	/*$('#openEventForm').click(function(){
-		$('#eventForm').slideToggle(200);
-	});
-*/
 
 });
