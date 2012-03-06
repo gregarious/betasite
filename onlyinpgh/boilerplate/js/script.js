9a84b52ed6bb0c537445d51e9ece5a3ec3a25e0a
laras126 <laras126@gmail.com>
2012-03-06 13:06:14 -0500

    header/bcast postitioning, add single place menu

--- onlyinpgh/boilerplate/js/script.js
+++ onlyinpgh/boilerplate/js/script.js
@@ -3,6 +3,16 @@ jQuery(document).ready( function($) {
 	// this doesn't get each one, just first first
 	/*$('.item').find('.tag-list ul li a').first().css('margin-left', '0');*/
 
+	// Highlight the current menu item based on URL.
+	// Alter this so it ignores id paths - i.e. Places is stil highlighted at /places/229
+
+	loc = location.pathname;
+	menu_item = $('#site-nav').find('a[href$="'+loc+'"]');
+	
+	if(menu_item.attr('href') == loc) {
+		menu_item.addClass('current-page');
+	}
+
 }); // document.ready
 
 
