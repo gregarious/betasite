1e7c1e24acba10da978c2ad16ba3b3b0a1fbd62f
Lara Schenck <Lara@Laras-MacBook-Air.local>
2012-03-11 13:45:16 -0400

    ills positioning, ready for slide

--- onlyinpgh/boilerplate/js/admin_script.js
+++ onlyinpgh/boilerplate/js/admin_script.js
@@ -1,4 +1,4 @@
-$(document).ready( function(){
+jQuery(document).ready( function(){
 
 	// slideToggle the add event/special forms
 	var expand_form = function(title, content) {
@@ -11,5 +11,38 @@ $(document).ready( function(){
 	expand_form('#openSpecialForm','#specialForm');
 	expand_form('.scheduled-item h4','.scheduled-item > .item-content');
 
+	// Sliding signup sequence
+
+	$('a.panel').click(function () {
+
+		$('a.panel').removeClass('selected');
+		$(this).addClass('selected');
+		
+		current = $(this);
+		
+		$('#wrapper').scrollTo($(this).attr('href'), 800);		
+		
+		return false;
+	});
+
+	$(window).resize(function () {
+		resizePanel();
+	});
+
 
 });
+
+function resizePanel() {
+
+	width = $(window).width();
+	height = $(window).height();
+
+	mask_width = width * $('.item').length;
+		
+	$('#debug').html(width  + ' ' + height + ' ' + mask_width);
+		
+	$('#wrapper, .item').css({width: width, height: height});
+	$('#mask').css({width: mask_width, height: height});
+	$('#wrapper').scrollTo($('a.selected').attr('href'), 0);
+		
+}
