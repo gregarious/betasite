def190e3c7066da89eabb38ebdf79f1949355a6e
Lara Schenck <Lara@new-host-3.home>
2012-03-12 15:14:29 -0400

    ignore settings, changed it

--- www/js/settings.js
+++ www/js/settings.js
@@ -9,7 +9,9 @@
 	// if true, various settings will be changed to allow debugging in the browser (e.g. JSONP requests)
 	// TODO: SHOULD BE FIXED TO FALSE (or all traces removed from source) FOR LAUNCH!
 	// total hack the way it's done here -- e.g. won't work for Android simulators
-	var BROWSER_DEBUG = navigator.userAgent.toLowerCase().search('iphone') === -1;
+	//var BROWSER_DEBUG = navigator.userAgent.toLowerCase().search('iphone') === -1;
+
+	var BROWSER_DEBUG = true;
 
 	var settings = obid.settings = {
 		// root url for API calls
