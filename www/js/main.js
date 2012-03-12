8c9c3c43d06869548d396e29e6f9b51458eecf36
Greg Nicholas <greg.nicholas@gmail.com>
2012-03-12 18:39:43 -0400

    nav header and showView helpers working

--- www/js/main.js
+++ www/js/main.js
@@ -36,71 +36,96 @@ $(function() {
         // since we use require below, we become asynchronous.
         // we need to return a promise for when we're done.
         var configuringApp = $.Deferred();
-        var app;    // initialized to AppRouter below
+        
         // TODO: make this more selective script loading. For now just load it all up front.
         require(
             ["models/places",
              "views/places_feed",
-             "views/place_detail"],
-            function(models,feed_views,detail_views) {
+             "views/place_detail",
+             "views/nav_header"],
+            function(models,feed_views,detail_views,header_views) {
+
+            // TODO: move into some backbone plugin/util places
+            var showView = function(selector,view) {
+                // TODO: save view connected to each selector
+                $(selector).html(view.render().el);
+                return view;
+            };
+
+            var fetchAndShow = function(selector,model,ViewClass,spinner) {
+                if(spinner) {
+                    $(selector).html('<div class="spinner">spinning...</div>');
+                }
+                model.fetch({
+                    // callback could return model or collection as first arg
+                    success: function(model,response) {
+                        showView(selector, new ViewClass({model: model}));
+                    },
+                    error: function(model,response) {
+                        $(selector).html('<p>error: ' + response.statusText+'</p>');
+                    }
+                });
+            };
 
             var AppRouter = Backbone.Router.extend({
                 routes: {
                     '':'home',
                     'places':'places_feed',
-                    'places/:id':'place_detail'
+                    'places/:id':'place_detail',
+                    'events':'events_feed',
+                    'events/:id':'event_detail',
+                    'specials':'specials_feed',
+                    'specials/:id':'specials_detail'
+                },
+
+                initialize: function(){
+                    showView('header',new header_views.NavHeader());
                 },
 
                 home: function(){
                     console.log('Router: home');
-                    // Temporary redirect
-                    this.navigate('places', true);
+                    // // Temporary redirect
+                    // this.navigate('places', true);
                 },
 
                 places_feed: function(){
                     console.log('Router: places_feed');
                     var feed = new models.PlacesFeed();
-                    console.log('spinner on');
-                    feed.fetch({
-                        success: function(collection,response) {
-                            console.log('spinner off');
-                            app.showView('#container',
-                                new feed_views.PlacesFeedView({model:collection}));
-                        },
-                        error: function(collection,response) {
-                            console.log('error! ' + response.statusText);
-                            console.log('spinner off');
-                        }
-                    });
+                    fetchAndShow('#container',feed,feed_views.PlacesFeedView,true);
+                    // feed.fetch({
+                    //     success: function(collection,response) {
+                    //         console.log('spinner off');
+                    //         showView('#container',
+                    //             new feed_views.PlacesFeedView({model:collection}));
+                    //     },
+                    //     error: function(collection,response) {
+                    //         console.log('error! ' + response.statusText);
+                    //         console.log('spinner off');
+                    //     }
+                    // });
                 },
 
                 place_detail: function(id){
                     console.log('Router: place_detail');
                     var place = new models.PlaceDetail({id:id});
 
-                    console.log('spinner on');
-                    place.fetch({
-                        success: function(model,response) {
-                            console.log('spinner off');
-                            app.showView('#container',
-                                new detail_views.PlaceDetailView({model:model}));
-                        },
-                        error: function(model,response) {
-                            console.log('error! ' + response.statusText);
-                            console.log('spinner off');
-                        }
-                    });
-                },
-
-                showView: function(selector, view) {
-                    this.currentView = view;
-                    $(selector).html(this.currentView.render().el);
-                    return view;
+                    fetchAndShow('#container',place,detail_views.PlaceDetailView,true);
+                    // place.fetch({
+                    //     success: function(model,response) {
+                    //         console.log('spinner off');
+                    //         showView('#container',
+                    //             new detail_views.PlaceDetailView({model:model}));
+                    //     },
+                    //     error: function(model,response) {
+                    //         console.log('error! ' + response.statusText);
+                    //         console.log('spinner off');
+                    //     }
+                    // });
                 }
             });
             
             // creating the new Router registers it with Backbone
-            app = new AppRouter();
+            window.app = new AppRouter();
 
             configuringApp.resolve();
         });
