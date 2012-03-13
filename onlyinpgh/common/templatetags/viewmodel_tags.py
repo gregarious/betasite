d9357fff9225ebbe0ed7843834a980345cbf13fe
Greg Nicholas <greg.nicholas@gmail.com>
2012-03-13 11:09:16 -0400

    cleaned up common package for Python linter

--- onlyinpgh/common/templatetags/viewmodel_tags.py
+++ onlyinpgh/common/templatetags/viewmodel_tags.py
@@ -1,13 +1,15 @@
 from django import template
 register = template.Library()
 
+
 @register.simple_tag(takes_context=True)
-def self_render(context,viewmodel):
-	# if this statment throws a KeyError, first ensure that the request template 
-	# processor is listed in settings.py under TEMPLATE_CONTEXT_PROCESSORS
-	# (see https://docs.djangoproject.com/en/dev/ref/templates/api/#django-core-context-processors-request)
-	return viewmodel.to_html(request=context['request'])
+def self_render(context, viewmodel):
+    # if this statment throws a KeyError, first ensure that the request template
+    # processor is listed in settings.py under TEMPLATE_CONTEXT_PROCESSORS
+    # (see https://docs.djangoproject.com/en/dev/ref/templates/api/#django-core-context-processors-request)
+    return viewmodel.to_html(request=context['request'])
+
 
 @register.simple_tag
 def self_render_nocontext(viewmodel):
-	return viewmodel.to_html()
+    return viewmodel.to_html()
--- onlyinpgh/common/viewmodels.py
+++ onlyinpgh/common/viewmodels.py
@@ -1,38 +1,41 @@
 from onlyinpgh.common.core.viewmodels import RenderableViewModel
 
+
 class FeedViewModel(RenderableViewModel):
-	'''
-	Base ViewModel for Feeds. Abstract base class, requires subclasses
-	to define a value for class_name.
-	'''
-	template_name = 'feed.html'
-	class_name = None
-	def __init__(self):
-		self.items = []
+    '''
+    Base ViewModel for Feeds. Abstract base class, requires subclasses
+    to define a value for class_name.
+    '''
+    template_name = 'feed.html'
+    class_name = None
 
-	def to_data(self,*args,**kwargs):
-		'''Lets base to_data to most of the work, then adds class_name to data.'''
-		cleaned_dict = super(FeedViewModel,self).to_data(*args,**kwargs)
-		cleaned_dict['class_name'] = self.class_name
-		return cleaned_dict
+    def __init__(self):
+        self.items = []
 
-	def to_html(self,request=None):
-		'''Ensures class name is defined'''
-		if not self.class_name:
-			raise NotImplementedError('FeedViewModel subclasses must define the class_name variable!')
-		return super(FeedViewModel,self).to_html(request)
+    def to_data(self, *args, **kwargs):
+        '''Lets base to_data to most of the work, then adds class_name to data.'''
+        cleaned_dict = super(FeedViewModel, self).to_data(*args, **kwargs)
+        cleaned_dict['class_name'] = self.class_name
+        return cleaned_dict
+
+    def to_html(self, request=None):
+        '''Ensures class name is defined'''
+        if not self.class_name:
+            raise NotImplementedError('FeedViewModel subclasses must define the class_name variable!')
+        return super(FeedViewModel, self).to_html(request)
+
 
 class FeedCollection(RenderableViewModel):
-	template_name = 'feed_collection.html'
-	def __init__(self,feed_tuples):
-		'''
-		Initialize from list of (label,FeedViewModel) tuples.
-		'''
-		self.feeds = [ {'label': label,
-						'feed_view': feed} 
-						for label,feed in feed_tuples ]
-		print 'FeedCollection init: ', self.__dict__
+    template_name = 'feed_collection.html'
 
-	def to_html(self,request=None):
-		print 'FeedCollection:', self.__dict__
-		return super(FeedCollection,self).to_html(request)
+    def __init__(self, feed_tuples):
+        '''
+        Initialize from list of (label,FeedViewModel) tuples.
+        '''
+        self.feeds = [{'label': label, 'feed_view': feed}
+                        for label, feed in feed_tuples]
+        print 'FeedCollection init: ', self.__dict__
+
+    def to_html(self, request=None):
+        print 'FeedCollection:', self.__dict__
+        return super(FeedCollection, self).to_html(request)
