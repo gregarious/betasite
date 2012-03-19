ffc277341294b79aced802cfcd8cbfab79c5c008
Greg Nicholas <greg.nicholas@gmail.com>
2012-03-19 18:59:57 -0400

    full view/template skeleton created for orgadmin

--- onlyinpgh/orgadmin/urls.py
+++ onlyinpgh/orgadmin/urls.py
@@ -1,10 +1,24 @@
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('',
-    url(r'^$', 'onlyinpgh.orgadmin.views.page_home'),
-    url(r'^home/$', 'onlyinpgh.orgadmin.views.page_home', name='orgadmin-home'),
+urlpatterns = patterns('onlyinpgh.orgadmin.views',
+    url(r'^$', 'page_home'),
+    url(r'^home/$', 'page_home', name='orgadmin-home'),
 
-    url(r'^signup/$', 'onlyinpgh.orgadmin.views.page_signup'),
-    url(r'^login/$', 'onlyinpgh.orgadmin.views.page_login', name='orgadmin-login'),
-    url(r'^logout/$', 'onlyinpgh.orgadmin.views.page_logout'),
+    url(r'^signup/$', 'page_signup'),
+    url(r'^login/$', 'page_login', name='orgadmin-login'),
+    url(r'^logout/$', 'page_logout'),
+
+    # handle new place creations, and place claiming with page_setup_place_wizard
+    url(r'^places/setup/$', 'page_setup_place_wizard'),
+    url(r'^places/setup/(?P<id>\d+)/$', 'page_setup_place_wizard'),
+
+    url(r'^places/edit/(?P<id>\d+)/$', 'page_edit_place'),
+    url(r'^places/list/$', 'page_list_places'),
+
+    url(r'^events/edit/(?P<id>\d+)/$', 'page_edit_event'),
+    url(r'^events/list/$', 'page_list_events'),
+
+    url(r'^specials/edit/(?P<id>\d+)/$', 'page_edit_special'),
+    url(r'^specials/list/$', 'page_list_specials'),
+
 )
--- onlyinpgh/orgadmin/views.py
+++ onlyinpgh/orgadmin/views.py
@@ -7,6 +7,7 @@ from django.forms.util import ErrorList
 from django.contrib.auth import login, authenticate, logout
 
 from onlyinpgh.orgadmin.forms import OrgSignupForm, OrgLoginForm
+from onlyinpgh.organizations.models import Organization
 
 
 def render_admin_page(safe_content, context_instance=None):
@@ -26,6 +27,9 @@ def response_admin_page(safe_content, context_instance=None):
 
 ### URL-linked page views ###
 def page_signup(request):
+    if request.user.is_authenticated():
+        logout(request)
+
     if request.POST:
         form = OrgSignupForm(request.POST)
         # test if the browser supports cookies
@@ -38,9 +42,15 @@ def page_signup(request):
         if form.is_valid():
             form.save()     # saves new user
 
-        # authenticate new user and log in
-        authenticate(username=form.username,)
-        login(request, form.get_user())
+        # authenticate new user, create  and log in
+        user = authenticate(username=form.username, password=form.password1)
+        login(request, user)
+
+        # create new organization for user to administer
+        org = Organization.objects.create(name=form.orgname)
+        org.administrators.add(user)
+
+        request.session['current_org'] = org
 
         # redirect to home page
         redirect_to = reverse('orgadmin-home')
@@ -57,6 +67,9 @@ def page_signup(request):
 
 
 def page_login(request):
+    if request.user.is_authenticated():
+        logout(request)
+
     if request.POST:
         # passing in request checks for cookies
         form = OrgLoginForm(request, data=request.POST)
@@ -64,7 +77,11 @@ def page_login(request):
             if request.session.test_cookie_worked():
                 request.session.delete_test_cookie()
 
-            login(request, form.get_user())
+            user = form.get_user()
+            login(request, user)
+
+            # just default to first org in the list for now
+            request.session['current_org'] = Organization.objects.filter(administrators=user)[0]
 
             # redirect to homepage
             redirect_to = reverse('orgadmin-home')
@@ -92,6 +109,95 @@ def page_home(request):
     if not request.user.is_authenticated():
         return HttpResponseRedirect(reverse('orgadmin-login'))
 
-    context = RequestContext(request)
+    context = RequestContext(request,
+        {'current_org': request.session['current_org']})
     content = render_to_string('orgadmin/home.html', context_instance=context)
+    return response_admin_page(content, context)
+
+
+def page_claim_place(request):
+    # must be authenticated to reach this page
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('orgadmin-login'))
+
+    context = RequestContext(request,
+        {'current_org': request.session['current_org']})
+    content = render_to_string('orgadmin/place_claim.html', context_instance=context)
+    return response_admin_page(content, context)
+
+
+def page_setup_place_wizard(request):
+    # must be authenticated to reach this page
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('orgadmin-login'))
+
+    context = RequestContext(request,
+        {'current_org': request.session['current_org']})
+    content = render_to_string('orgadmin/place_setup_wizard.html', context_instance=context)
+    return response_admin_page(content, context)
+
+
+def page_edit_place(request, id):
+    # must be authenticated to reach this page
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('orgadmin-login'))
+
+    context = RequestContext(request,
+        {'current_org': request.session['current_org']})
+    content = render_to_string('orgadmin/place_edit_form.html', context_instance=context)
+    return response_admin_page(content, context)
+
+
+def page_list_places(request):
+    # must be authenticated to reach this page
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('orgadmin-login'))
+
+    context = RequestContext(request,
+        {'current_org': request.session['current_org']})
+    content = render_to_string('orgadmin/place_list.html', context_instance=context)
+    return response_admin_page(content, context)
+
+
+def page_edit_event(request, id):
+    # must be authenticated to reach this page
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('orgadmin-login'))
+
+    context = RequestContext(request,
+        {'current_org': request.session['current_org']})
+    content = render_to_string('orgadmin/event_edit_form.html', context_instance=context)
+    return response_admin_page(content, context)
+
+
+def page_list_events(request):
+    # must be authenticated to reach this page
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('orgadmin-login'))
+
+    context = RequestContext(request,
+        {'current_org': request.session['current_org']})
+    content = render_to_string('orgadmin/event_list.html', context_instance=context)
+    return response_admin_page(content, context)
+
+
+def page_edit_special(request, id):
+    # must be authenticated to reach this page
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('orgadmin-login'))
+
+    context = RequestContext(request,
+        {'current_org': request.session['current_org']})
+    content = render_to_string('orgadmin/special_edit_form.html', context_instance=context)
+    return response_admin_page(content, context)
+
+
+def page_list_specials(request):
+    # must be authenticated to reach this page
+    if not request.user.is_authenticated():
+        return HttpResponseRedirect(reverse('orgadmin-login'))
+
+    context = RequestContext(request,
+        {'current_org': request.session['current_org']})
+    content = render_to_string('orgadmin/special_list.html', context_instance=context)
     return response_admin_page(content, context)
