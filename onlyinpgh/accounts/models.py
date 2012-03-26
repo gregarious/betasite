8b9b9bcd9e6a276b07f1bc2285b05e8ae49b5b46
Greg Nicholas <greg.nicholas@gmail.com>
2012-03-26 17:54:53 -0400

    UserProfile modified, profile mockup data added

--- onlyinpgh/accounts/models.py
+++ onlyinpgh/accounts/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from onlyinpgh.common.core.viewmodels import ViewModel
 
 
-class UserProfile(models.Model):
+class UserProfile(models.Model, ViewModel):
     '''
     Extended information for any user of the site.
     '''
@@ -20,14 +21,32 @@ class UserProfile(models.Model):
 
     # all of the following should be optional on a registration form
     fb_id = models.CharField(max_length=50, blank=True)
+    fb_id_public = models.BooleanField(default=True)
+
     twitter_username = models.CharField(max_length=15, blank=True)
+    twitter_username_public = models.BooleanField(default=True)
 
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
     birth_year = models.IntegerField(null=True, blank=True)
+
     neighborhood = models.CharField(max_length=50, blank=True)
 
     def __unicode__(self):
         return unicode(self.user)
+
+    def to_data(self, *args, **kwargs):
+        '''
+        Manually choose and insert user information. Note that
+        two ids are produced in the data output: id (User.id)
+        and profile_id (UserProfile.id).
+        '''
+        data = super(UserProfile, self).to_data(*args, **kwargs)
+        data['profile_id'] = self.id
+        data.pop('user_id')
+        user_fields = ('id', 'username', 'email')
+        for k in user_fields:
+            data[k] = getattr(self.user, k)
+        return data
 
 
 # signal handler to automatically create a new UserProfile
