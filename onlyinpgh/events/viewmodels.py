c8a108248ce8a37f11549b4414c9ddb2e8a49df1
Greg Nicholas <greg.nicholas@gmail.com>
2012-03-26 16:38:12 -0400

    attendance status in event viewmodels

--- onlyinpgh/events/viewmodels.py
+++ onlyinpgh/events/viewmodels.py
@@ -19,6 +19,7 @@ class EventFeedItem(ViewModel):
             [tags]
                 id
                 name
+        is_attending (True)
     '''
     def __init__(self, event, user=None):
         super(EventFeedItem, self).__init__()
@@ -38,6 +39,9 @@ class EventFeedItem(ViewModel):
             for k in place_data.keys():
                 if k not in keepers:
                     place_data.pop(k)
+
+        data['is_attending'] = self.event.attendee_set.\
+                                filter(is_attending=True).count() > 0
 
         return data
 
@@ -62,6 +66,7 @@ class EventDetail(ViewModel):
                 id
                 name
             invisible   (boolean)
+        is_attending (True)
     '''
     def __init__(self, event, user=None):
         super(EventDetail, self).__init__()
@@ -69,9 +74,12 @@ class EventDetail(ViewModel):
 
     def to_data(self, *args, **kwargs):
         data = super(EventDetail, self).to_data(*args, **kwargs)
-        place_data = data['event'].get('place')
-        if place_data:
-            for k in place_data.keys():
+        event_data = data['event'].get('place')
+        if event_data:
+            for k in event_data.keys():
                 if k not in ('id', 'name'):
-                    place_data.pop(k)
+                    event_data.pop(k)
+        data['is_attending'] = self.event.attendee_set.\
+                        filter(is_attending=True).count() > 0
+
         return data
