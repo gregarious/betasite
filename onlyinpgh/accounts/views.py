a5e319a2b080262fc581420f6599fbab9f9c8d12
Greg Nicholas <greg.nicholas@gmail.com>
2012-03-26 00:18:23 -0400

    ported over registration/login/logout views

--- onlyinpgh/accounts/views.py
+++ onlyinpgh/accounts/views.py
@@ -2,17 +2,59 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template import RequestContext
 from django.template.loader import render_to_string
-from onlyinpgh.accounts.forms import RegistrationForm, UserProfileForm
+from django.utils.safestring import mark_safe
 
 from django.core.urlresolvers import reverse
-from django.contrib.auth import login, authenticate
+
+from django.contrib.auth import login, authenticate, REDIRECT_FIELD_NAME
+from django.contrib.auth.forms import AuthenticationForm
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+
+from onlyinpgh.accounts.forms import RegistrationForm, UserProfileForm
+from onlyinpgh.common.views import page_response, render_main
+import urlparse
 
 
-# TODO: create this view once there's some time. default django view is ok,
-# but it doesn't test for cookies, is kind of awkward to use with our
-# module-based page building, etc.
-def page_login(request):
-    pass
+@csrf_protect
+@never_cache
+def page_login(request, redirect_field_name=REDIRECT_FIELD_NAME):
+    '''
+    Renders login page.
+    '''
+    redirect_to = request.REQUEST.get(redirect_field_name, '')
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            netloc = urlparse.urlparse(redirect_to)[1]
+
+            # Use default setting if redirect_to is empty
+            if not redirect_to:
+                redirect_to = reverse('home')
+
+            # Security check -- don't allow redirection to a different
+            # host.
+            elif netloc and netloc != request.get_host():
+                redirect_to = reverse('home')
+
+            # Okay, security checks complete. Log the user in.
+            login(request, form.get_user())
+
+            if request.session.test_cookie_worked():
+                request.session.delete_test_cookie()
+
+            return HttpResponseRedirect(redirect_to)
+    else:
+        form = AuthenticationForm(request)
+    request.session.set_test_cookie()
+
+    # render login form with csrf protection
+    login_form = render_to_string('registration/login.html',
+        {'form': form, 'form_action': reverse('login')},
+        context_instance=RequestContext(request))
+    main_content = render_main(mark_safe(login_form))
+    return page_response(main_content, request)
 
 
 def page_signup(request):
@@ -49,10 +91,9 @@ def page_signup(request):
         profile_form = UserProfileForm(prefix='prof')
 
     request.session.set_test_cookie()
-    context = RequestContext(request)
-    content = render_to_string('registration/signup_form.html',
+    content = render_to_string('registration/signup.html',
         {'registration_form': reg_form, 'profile_form': profile_form,
-         'form_action': reverse('login')},
-        context_instance=context)
-
-    return render(request, 'page.html', {'main_content': content})
+         'form_action': reverse('signup')},
+        context_instance=RequestContext(request))
+    content = mark_safe(content)
+    return page_response(content, request)
--- onlyinpgh/urls.py
+++ onlyinpgh/urls.py
@@ -7,11 +7,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'onlyinpgh.views.page_home', name='home'),
+    url(r'^$', 'onlyinpgh.common.views.page_home', name='home'),
 
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^login/$', 'onlyinpgh.accounts.views.page_login', name='login'),
     url(r'^signup/$', 'onlyinpgh.accounts.views.page_signup', name='signup'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout', kwargs={'next_page':'/'}),
 
     url(r'^admin/', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
