12ea840f0d91933f68af59fdfcdde41dbbe5aed5
Greg Nicholas <greg.nicholas@gmail.com>
2012-04-02 15:44:04 -0500

    removed accidental leading slashes in urls

--- onlyinpgh/urls.py
+++ onlyinpgh/urls.py
@@ -10,21 +10,21 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^$', redirect_to, {'url': 'http://scenable.com/blog/'}),
 
-    url(r'^/prelaunch/$', 'onlyinpgh.common.views.page_home', name='home'),
-    url(r'^/prelaunch/login/$', 'onlyinpgh.accounts.views.page_login', name='login'),
-    url(r'^/prelaunch/signup/$', 'onlyinpgh.accounts.views.page_signup', name='signup'),
-    url(r'^/prelaunch/logout/$', 'django.contrib.auth.views.logout', name='logout', kwargs={'next_page': '/prelaunch/'}),
+    url(r'^prelaunch/$', 'onlyinpgh.common.views.page_home', name='home'),
+    url(r'^prelaunch/login/$', 'onlyinpgh.accounts.views.page_login', name='login'),
+    url(r'^prelaunch/signup/$', 'onlyinpgh.accounts.views.page_signup', name='signup'),
+    url(r'^prelaunch/logout/$', 'django.contrib.auth.views.logout', name='logout', kwargs={'next_page': '/prelaunch/'}),
 
-    url(r'^/prelaunch/account/', include('onlyinpgh.accounts.urls')),
+    url(r'^prelaunch/account/', include('onlyinpgh.accounts.urls')),
 
-    url(r'^/prelaunch/admin/', include(admin.site.urls)),
+    url(r'^prelaunch/admin/', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^/prelaunchadmin/doc/', include('django.contrib.admindocs.urls')),
 
-    url(r'^/prelaunch/places/', include('onlyinpgh.places.urls')),
-    url(r'^/prelaunch/events/', include('onlyinpgh.events.urls')),
-    url(r'^/prelaunch/specials/', include('onlyinpgh.specials.urls')),
-    url(r'^/prelaunch/hot/$', 'onlyinpgh.hot.views.page_hot', name='hot'),
+    url(r'^prelaunch/places/', include('onlyinpgh.places.urls')),
+    url(r'^prelaunch/events/', include('onlyinpgh.events.urls')),
+    url(r'^prelaunch/specials/', include('onlyinpgh.specials.urls')),
+    url(r'^prelaunch/hot/$', 'onlyinpgh.hot.views.page_hot', name='hot'),
 
     #url(r'^tags/',include('onlyinpgh.tags.urls')),
 
