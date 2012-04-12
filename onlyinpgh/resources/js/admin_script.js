bf2ec34dcbe773235e1c42c828a954728944cd16
Lara <laras126@gmail.com>
2012-04-12 11:21:46 -0400

    current menu item

--- onlyinpgh/resources/js/admin_script.js
+++ onlyinpgh/resources/js/admin_script.js
@@ -1,10 +1,9 @@
-
 jQuery(document).ready( function(){
     // Copied from script.js - highlight current menu item based on URL
     loc = location.pathname;
     menu_item = $('#page-nav').find('a[href$="'+loc+'"]');
     
-    if(menu_item.attr('href') == loc + '/list') {
+    if(menu_item.attr('href') == loc) {
         menu_item.addClass('current-page');
     }
 });
