62ed69e76e9a41a7e539cf4d182954fd66b3099f
Lara <laras126@gmail.com>
2012-04-13 11:57:48 -0400

    fix missing quote in claim place ac img alt

--- onlyinpgh/resources/js/orgadmin/claimplace.js
+++ onlyinpgh/resources/js/orgadmin/claimplace.js
@@ -73,7 +73,7 @@ $(function(){
                         return {
                             label: '<div class="ac-item">' +
                                     '<div class="item-thumb">' +
-                                        '<img src=' + item.image_url + ' alt="' + item.name + ' height="50" width="50" />' +
+                                        '<img src=' + item.image_url + ' alt="' + item.name + '" height="50" width="50" />' +
                                     '</div>' + 
                                     '<div class="item-content">' +
                                         '<h4 class="item-title">'+ item.name + '</h4>' +
