7cd5b9b954a31cd6efffd70ff7d0af29b575e7db
Greg Nicholas <greg.nicholas@gmail.com>
2012-04-29 16:37:54 -0400

    adjustments to news/chatter

--- onlyinpgh/news/admin.py
+++ onlyinpgh/news/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from onlyinpgh.news.models import Article
 
-# renable after app has been added back to project
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['title', 'publication_date']
 
