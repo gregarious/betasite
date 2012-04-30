3525c3d1922c7a553e74e4fe4ffc00810d5b3a43
Greg Nicholas <greg.nicholas@gmail.com>
2012-04-30 18:43:55 -0400

    basic search indices created for all apps

--- onlyinpgh/places/search_indexes.py
+++ onlyinpgh/places/search_indexes.py
@@ -2,7 +2,7 @@ from haystack import indexes
 from onlyinpgh.places.models import Place
 
 
-class PlaceIndex(indexes.SearchIndex, indexes.Indexable):
+class PlaceTextOnlyIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     def get_model(self):
