52d45e95b70fb3f1f339f1bbc903341d60027734
Greg Nicholas <greg.nicholas@gmail.com>
2012-05-02 01:12:01 -0400

    restricted now objects to listed ones

--- onlyinpgh/now/views.py
+++ onlyinpgh/now/views.py
@@ -16,8 +16,8 @@ import random
 def page_now(request):
     # TODO: logic to pick relevant items
     objects = []
-    objects += random.sample(Place.objects.all(), min(4, Place.objects.count()))
-    objects += random.sample(Event.objects.all(), min(4, Event.objects.count()))
+    objects += random.sample(Place.listed_objects.all(), min(4, Place.objects.count()))
+    objects += random.sample(Event.listed_objects.all(), min(4, Event.objects.count()))
     objects += random.sample(Special.objects.all(), min(4, Special.objects.count()))
     random.shuffle(objects)
 
