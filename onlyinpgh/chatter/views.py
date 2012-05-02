11e7d64f34d13f9fe4de459cf127fcb8c3cd55ee
Greg Nicholas <greg.nicholas@gmail.com>
2012-05-02 01:06:03 -0400

    chatter posting in place

--- onlyinpgh/chatter/views.py
+++ onlyinpgh/chatter/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render_to_response
 from onlyinpgh.common.views import PageContext
 from onlyinpgh.chatter.models import Post
+from onlyinpgh.chatter.forms import PostForm
 
 
 def page_feed(request):
+    if request.POST:
+        submit_form = PostForm(author=request.user, data=request.POST)
+        if submit_form.is_valid():
+            if submit_form.cleaned_data['content'].strip() != '':
+                submit_form.save()
+    else:
+        submit_form = PostForm(author=request.user)
+
     items = Post.objects.all().order_by('-dtcreated')
     context = PageContext(request,
         current_section='chatter',
         page_title='Scenable | Chatter Feed',
-        content_dict={'items': items})
+        content_dict={'items': items, 'form': submit_form})
     return render_to_response('chatter/page_feed.html', context_instance=context)
