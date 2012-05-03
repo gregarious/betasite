260bed7a9011e4970427b0cb1a54bf4ebb7b8d4b
Greg Nicholas <greg.nicholas@gmail.com>
2012-05-03 11:07:05 -0400

    chatter posts using avatar now

--- onlyinpgh/accounts/viewmodels.py
+++ onlyinpgh/accounts/viewmodels.py
@@ -3,7 +3,7 @@ class PublicProfile(object):
     For now, just a copy of a UserProfile, but this will change.
     '''
     def __init__(self, user, **kwargs):
-        fields = ('display_name', 'avatar_url', 'points', 'fb_id',
+        fields = ('display_name', 'avatar', 'points', 'fb_id',
                   'fb_id_public', 'twitter_username', 'neighborhood',
                   'twitter_username_public', 'gender', 'birth_year')
         profile = user.get_profile()
