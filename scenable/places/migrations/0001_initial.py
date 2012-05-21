3d3534a8aba0d8b1985d0c543fb429b2d12c898b
Greg Nicholas <greg.nicholas@gmail.com>
2012-05-21 11:32:41 -0400

    added dependencies to initial migrations

--- scenable/places/migrations/0001_initial.py
+++ scenable/places/migrations/0001_initial.py
@@ -6,8 +6,11 @@ from django.db import models
 
 class Migration(SchemaMigration):
 
+    depends_on = (
+        ('tags', '0001_initial'),
+    )
+
     def forwards(self, orm):
-        
         # Adding model 'Location'
         db.create_table('places_location', (
             ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
