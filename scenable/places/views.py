178b5f35656fe6fadcf5cde40df1ba479d612e62
Greg Nicholas <greg.nicholas@gmail.com>
2012-05-25 19:09:10 -0400

    temporary places code to prove elasticsearch works

--- scenable/places/views.py
+++ scenable/places/views.py
@@ -4,43 +4,123 @@ from django.contrib.auth.decorators import login_required
 
 #from scenable.common.utils.jsontools import serialize_resources, jsonp_response, sanitize_json
 from scenable.common.views import PageContext, PageFilteredFeed
+from scenable.common.utils.jsontools import sanitize_json
 
 from scenable.places.models import Place
 # from scenable.places.resources import PlaceFeedResource
 from scenable.places.viewmodels import PlaceData, PlaceRelatedFeeds
 
 from haystack.forms import SearchForm
+from haystack.views import SearchView
+from haystack.query import SearchQuerySet
+
+import json
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-class PagePlacesFeed(PageFilteredFeed):
+class PagePlacesFeed(SearchView):
     def __init__(self, *args, **kwargs):
+        self.search_used = False
         super(PagePlacesFeed, self).__init__(
-            model_class=Place,
-            viewmodel_class=PlaceData,
             template='places/page_feed.html',
             form_class=SearchForm,
             results_per_page=8,
+            searchqueryset=SearchQuerySet().all().models(Place)
         )
 
-    def get_page_context(self, content):
-        return PageContext(self.request,
+    def get_results(self):
+        """
+        If a query exists, fetch results via the form. Otherwise Fetches the results via the form.
+
+        Returns an empty list if there's no query to search with.
+        """
+        if self.query:
+            self.search_used = True
+            return self.form.search()
+        else:
+            self.search_used = False
+            return Place.listed_objects.all()
+
+    def build_page(self):
+        """
+        Paginates the results appropriately. Pages will contains an actual
+        model object, despite ragardless of whether the results returned
+        were model instances or search results.
+        """
+        if self.search_used:
+            processed_results = [PlaceData(result.object) for result in self.results]
+        else:
+            processed_results = [PlaceData(result) for result in self.results]
+
+        paginator = Paginator(processed_results, self.results_per_page)
+
+        page_no = self.request.GET.get('p')
+        try:
+            page = paginator.page(page_no)
+        except PageNotAnInteger:
+            page = paginator.page(1)
+        except EmptyPage:
+            # if page out of range, return last one
+            page = paginator.page(paginator.num_pages)
+        return page
+
+    def create_response(self):
+        page = self.build_page()
+
+        content = {
+            'query': self.query,
+            'form': self.form,
+            'page': page,
+            'items_json': sanitize_json(json.dumps([p.serialize() for p in page])),
+        }
+
+        context = PageContext(self.request,
             current_section='places',
-            page_title='Scenable | Oakland Places',
-            content_dict=content)
+            page_title='Scenable | Oakland Places')
+        return render_to_response(self.template, content, context_instance=context)
 
-    def hacked_unfiltered(self):
-        '''
-        Temporary hack to get a list of unfiltered results. Will either be
-        self.model_class.objects.all() or
-        self.model_class.listed_objects.all().
 
-        Necessary because self.searchqueryset.all() seems to be buggy.
-        '''
-        places = [p for p in Place.listed_objects.all()]
-        return sorted(places, key=lambda p: -p.favorite_set.count())
+# class PagePlacesFeed(PageFilteredFeed):
+#     def __init__(self, *args, **kwargs):
+#         super(PagePlacesFeed, self).__init__(
+#             model_class=Place,
+#             viewmodel_class=PlaceData,
+#             template='places/page_feed.html',
+#             form_class=SearchForm,
+#             results_per_page=8,
+#         )
 
-    def hacked_filtered(self):
-        return [result.object for result in self.form.search()]
+#     def create_response(self):
+#         (paginator, page) = self.build_page()
+
+#         places = [PlaceData(result.object) for result in page.object_list]
+
+#         context = {
+#             'query': self.query,
+#             'form': self.form,
+#             'page': page,
+#             'items_json': sanitize_json(json.dumps([p.serialize() for p in places])),
+#         }
+
+#     def get_page_context(self, content):
+#         return PageContext(self.request,
+#             current_section='places',
+#             page_title='Scenable | Oakland Places',
+#             content_dict=content)
+
+#     def hacked_unfiltered(self):
+#         '''
+#         Temporary hack to get a list of unfiltered results. Will either be
+#         self.model_class.objects.all() or
+#         self.model_class.listed_objects.all().
+
+#         Necessary because self.searchqueryset.all() seems to be buggy.
+#         '''
+#         places = [p for p in Place.listed_objects.all()]
+#         return sorted(places, key=lambda p: -p.favorite_set.count())
+
+#     def hacked_filtered(self):
+#         return [result.object for result in self.form.search()]
 
 
 @login_required
